f320c3a759a05b6e1aa39bc367573ca309a358f2
QuanticPotato <barnabe.chauvin@gmail.com>
2015-08-19 15:43:01 +0200

    [Cakephp] Fix type.

--- cakephp/install.sh
+++ cakephp/install.sh
@@ -2,6 +2,6 @@
 
 echo "root=" > /etc/ssmtp/ssmtp.conf
 echo "AuthUser=$smtp_user" >> /etc/ssmtp/ssmtp.conf
-echo "AuthUser=$smtp_password" >> /etc/ssmtp/ssmtp.conf
+echo "AuthPass=$smtp_password" >> /etc/ssmtp/ssmtp.conf
 echo "mailhub=mail" >> /etc/ssmtp/ssmtp.conf
 echo "hostname=$(hostname)" >> /etc/ssmtp/ssmtp.conf
